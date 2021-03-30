46451e6aad51f5e24daf22a4835de9e66c9c674e
Денис <dultsevdv@gmail.com>
2021-03-31 03:59:25 +0500

    Кривые листбоксы

---
+++ WPR.Styles/Styles/ListBoxes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using WPR.Styles.Services;
+
+namespace WPR.Styles.Styles
+{
+    partial class ListBoxes
+    {
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e) 
+        {
+            if(sender is not ListBoxItem item) return;
+            ColorAnimation animation = new()
+            {
+                To = Design.GetBrushFromResource( Design.StyleBrush.TextColorBrush).Color,
+                Duration = TimeSpan.FromSeconds(0.2),
+            };
+            Storyboard.SetTargetProperty(animation, new PropertyPath(Control.ForegroundProperty));
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(animation);
+            storyboard.Begin(item);
+        }
+
+        private void ListBoxItem_UnSelected(object sender, RoutedEventArgs e)
+        {
+            if (sender is not ListBoxItem item) return;
+            ColorAnimation animation = new()
+            {
+                Duration = TimeSpan.FromSeconds(0.2),
+            };
+            Storyboard.SetTargetProperty(animation, new PropertyPath(Control.ForegroundProperty));
+            Storyboard storyboard = new();
+            storyboard.Children.Add(animation);
+            storyboard.Begin(item);
+        }
+    }
+}
