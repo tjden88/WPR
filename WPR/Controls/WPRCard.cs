b2a3ded05b9a2813e7fb10a4899c929888529364
Денис <dultsevdv@gmail.com>
2021-04-03 16:09:40 +0500

    WPRCard

---
+++ WPR/Controls/WPRCard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPR.Controls
+{
+    /// <summary>
+    /// Карточка для отображения контента с тенью
+    /// </summary>
+    public class WPRCard : ContentControl
+    {
+        static WPRCard()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WPRCard), new FrameworkPropertyMetadata(typeof(WPRCard)));
+        }
+
+
+        /// <summary>
+        /// Жирная тень для всплывающего окна
+        /// </summary>
+        public bool IsPopupShadowStyle
+        {
+            get => (bool)GetValue(IsPopupShadowStyleProperty);
+            set => SetValue(IsPopupShadowStyleProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for IsPopupShadowStyle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsPopupShadowStyleProperty =
+            DependencyProperty.Register("IsPopupShadowStyle", typeof(bool), typeof(WPRCard), new PropertyMetadata(false));
+
+
+    }
+}
