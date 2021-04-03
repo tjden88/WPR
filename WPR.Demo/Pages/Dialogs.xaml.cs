1ecb05b3bf11b449aad969e30cb8d27282475354
Денис <dultsevdv@gmail.com>
2021-04-03 16:03:36 +0500

    Диалог редактирования

---
+++ WPR.Demo/Pages/Dialogs.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPR.Demo.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Dialogs.xaml
+    /// </summary>
+    public partial class Dialogs : Page
+    {
+        public Dialogs()
+        {
+            InitializeComponent();
+        }
+    }
+}
