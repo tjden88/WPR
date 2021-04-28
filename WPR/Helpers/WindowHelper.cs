e7175643917e84584ddf65566c8b6b998db9249e
Денис <dultsevdv@gmail.com>
2021-04-29 00:02:41 +0500

    Правки комбобокса и окна

--- WPR/Helpers/WindowHelper.cs
+++ WPR/Helpers/WindowHelper.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Media;
 
 namespace WPR.Helpers
 {
     public static class WindowHelper
     {
-        /// <summary>
-        /// Отступ строки заголовка от левого края
-        /// </summary>
+        #region  Отступ строки заголовка от левого края 
+        /// <summary> Отступ строки заголовка от левого края </summary>
         public static readonly DependencyProperty TitleLeftMarginProperty =
             DependencyProperty.RegisterAttached("TitleLeftMargin", typeof(double), typeof(WindowHelper), new PropertyMetadata(2.0));
 
@@ -23,10 +17,12 @@ namespace WPR.Helpers
 
         public static double GetTitleLeftMargin(DependencyObject obj)
         {
-            return (double?) obj?.GetValue(TitleLeftMarginProperty) ?? 0.0;
+            return (double?)obj?.GetValue(TitleLeftMarginProperty) ?? 0.0;
         }
 
+        #endregion
 
+        #region Цвет текста строки заголовка окна
         public static SolidColorBrush GetWindowHeaderForeground(DependencyObject obj)
         {
             return (SolidColorBrush)obj.GetValue(WindowHeaderForegroundProperty);
@@ -37,10 +33,10 @@ namespace WPR.Helpers
             obj.SetValue(WindowHeaderForegroundProperty, value);
         }
 
-        /// <summary>
-        /// Цвет текста строки заголовка окна
-        /// </summary>
+        /// <summary> Цвет текста строки заголовка окна </summary>
         public static readonly DependencyProperty WindowHeaderForegroundProperty =
-            DependencyProperty.RegisterAttached("WindowHeaderForeground", typeof(SolidColorBrush), typeof(WindowHelper), new PropertyMetadata(null));
+            DependencyProperty.RegisterAttached("WindowHeaderForeground", typeof(SolidColorBrush), typeof(WindowHelper), new PropertyMetadata(null)); 
+        #endregion
+
     }
 }
