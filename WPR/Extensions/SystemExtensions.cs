b83eff95d212427f21fc2bcdaa306ceaa02f3cc2
Денис <dultsevdv@gmail.com>
2021-05-04 19:07:24 +0500

    Добавлены системные расширения

--- WPR/Extensions/SystemExtensions.cs
+++ WPR/Extensions/SystemExtensions.cs
@@ -8,13 +8,39 @@ namespace System
         /// <summary> Поиск визуального родителя по типу </summary>
         /// <typeparam name="T">Тип искомого родительского элемента</typeparam>
         /// <param name="child">Объект, родителя которого надо найти</param>
-        /// <returns></returns>
         public static T FindVisualParent<T>(this DependencyObject child) where T : DependencyObject
         {
             DependencyObject parentObject = VisualTreeHelper.GetParent(child);
             if (parentObject == null) return null;
             return parentObject is T parent? parent: FindVisualParent<T>(parentObject);
 
+        }
+
+        /// <summary> Поиск логического родителя по типу </summary>
+        /// <typeparam name="T">Тип искомого родительского элемента</typeparam>
+        /// <param name="child">Объект, родителя которого надо найти</param>
+        public static T FindLogicalParent<T>(this DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parentObject = LogicalTreeHelper.GetParent(child);
+            if (parentObject == null) return null;
+            return parentObject is T parent ? parent : FindLogicalParent<T>(parentObject);
+
+        }
+
+        /// <summary> Поиск визуального потомка по типу </summary>
+        /// <typeparam name="T">Тип искомого элемента</typeparam>
+        public static T FindVisualChild<T>(this DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null) return null;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+
+                var result = (child as T) ?? FindVisualChild<T>(child);
+                if (result != null) return result;
+            }
+            return null;
         }
 
         /// <summary>
@@ -35,6 +61,16 @@ namespace System
         public static int IntValue(this object exp)
         {
             return WPR.Work.VaInt(exp);
+        }
+
+        /// <summary>
+        /// Обмен значений 2 переменных
+        /// </summary>
+        public static void Swap<T>(this ref T first, ref T second) where T: struct
+        {
+            T tmp = second;
+            second = first;
+            first = tmp;
         }
     }
 }
