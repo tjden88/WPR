696ef1a97a0179bb7827cf318247059cb7894606
Денис <dultsevdv@gmail.com>
2021-09-13 05:14:08 +0500

    Слияние проектов

---
+++ WPR/MVVM/Commands/BaseCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPR.MVVM.Commands
+{
+    public abstract class BaseCommand : ICommand
+    {
+        private bool _Executable = true;
+
+        /// <summary>Ручная возможность выполнения команды</summary>
+        public bool Executable
+        {
+            get => _Executable;
+            set
+            {
+                if (_Executable == value) return;
+                _Executable = value;
+                ExecutableChanged?.Invoke(this, EventArgs.Empty);
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        /// <summary>Происходит при изменении ручной возможности исполнения команды</summary>
+        public event EventHandler ExecutableChanged;
+
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        bool ICommand.CanExecute(object parameter) => _Executable && CanExecute(parameter);
+
+        void ICommand.Execute(object parameter)
+        {
+            if (!((ICommand)this).CanExecute(parameter)) return;
+            Execute(parameter);
+        }
+
+        ///// <summary> Выполнить команду без параметра </summary>
+        public virtual void Execute() => Execute(null);
+
+        /// <summary>Возможность выполнения команды</summary>
+        public virtual bool CanExecute(object p) => true;
+
+        /// <summary>Выполнить команду с параметром</summary>
+        public abstract void Execute(object p);
+    }
+
+}
---
+++ WPR/MVVM/Converters/ValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace WPR.MVVM.Converters
+{
+    /// <summary>Базовая реализация конвертера</summary>
+    public class ValueConverter : ConverterBase
+    {
+        private readonly Func<object, Type, object, CultureInfo, object> _ConvertFunction;
+        private readonly Func<object, Type, object, CultureInfo, object> _ConvertBackFunction;
+
+        public ValueConverter(Func<object, Type, object, CultureInfo, object> ConvertFunction, Func< object, Type, object, CultureInfo, object> ConvertBackFunction = null)
+        {
+            _ConvertFunction = ConvertFunction;
+            _ConvertBackFunction = ConvertBackFunction;
+        }
+
+        protected override object Convert(object v, Type t, object p, CultureInfo c)
+        {
+            return _ConvertFunction?.Invoke(v, t, p, c);
+        }
+
+        protected override object ConvertBack(object v, Type t, object p, CultureInfo c)
+        {
+            return _ConvertFunction?.Invoke(v, t, p, c) ?? base.ConvertBack(v, t, p, c);
+        }
+    }
+}
