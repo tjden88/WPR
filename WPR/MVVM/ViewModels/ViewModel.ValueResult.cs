3f404bb731e152add71142478bc12a6d7434cb94
Денис <dultsevdv@gmail.com>
2021-09-25 19:52:35 +0500

    Больше сахара VM!

---
+++ WPR/MVVM/ViewModels/ViewModel.ValueResult.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace WPR.MVVM.ViewModels
+{
+    partial class ViewModel
+    {
+        public readonly ref struct ValueResult<T>
+        {
+            private readonly bool _Result;
+            private readonly T _NewValue;
+            private readonly ViewModel _Model;
+
+            public ValueResult(bool Result, in T NewValue, ViewModel Model)
+            {
+                _Result = Result;
+                _NewValue = NewValue;
+                _Model = Model;
+            }
+
+            public ValueResult<T> Then(Action Action)
+            {
+                if (_Result) Action();
+                return this;
+            }
+
+            public ValueResult<T> Then(Action<T> Action)
+            {
+                if (_Result) Action(_NewValue);
+                return this;
+            }
+
+            public ValueResult<T> ThenIf(Predicate<T> ConditionPredicate, Action Action)
+            {
+                if (_Result && ConditionPredicate(_NewValue))
+                    Action();
+                return this;
+            }
+
+            public ValueResult<T> ThenIf(Predicate<T> ConditionPredicate, Action<T> Action)
+            {
+                if (_Result && ConditionPredicate(_NewValue))
+                    Action(_NewValue);
+                return this;
+            }
+
+            public ValueResult<T> CallPropertyChanged(string PropertyName)
+            {
+                if (_Result) _Model.OnPropertyChanged(PropertyName);
+                return this;
+            }
+
+            public ValueResult<T> CallAllPropertiesChanged()
+            {
+                if (_Result) _Model.OnAllPropertiesChanged();
+                return this;
+            }
+        }
+    }
+}
