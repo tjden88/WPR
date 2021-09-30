cf5617385c54194e19a0b03b36b5162447a9ddba
Денис <dultsevdv@gmail.com>
2021-10-01 00:06:27 +0500

    Расширение функциональномти bool2vis converter

--- WPR/Converters/BoolToVisibilityConverter.cs
+++ WPR/Converters/BoolToVisibilityConverter.cs
@@ -7,23 +7,26 @@ using WPR.MVVM.Converters;
 namespace WPR.Converters
 {
     /// <summary>
-    /// Преобразовывает bool в Visible, если value=true, иначе Collapsed
+    /// Преобразовывает bool в Visible, если value=true, иначе - значение свойства HiddenVisibility (по умолчанию Collapsed)
     /// Если Parameter = !, то всё наоборот делает
     /// </summary>
     [ValueConversion(typeof(bool?), typeof(Visibility))]
     public class BoolToVisibilityConverter : ConverterBase
     {
+        /// <summary> Результат преобразования (по умолчанию Collapsed) </summary>
+        public Visibility HiddenVisibility { get; set; } = Visibility.Collapsed;
+
         protected override object Convert(object v, Type t, object p, CultureInfo c)
         {
-            if ((p as string) == "!")
-                return (bool) v ? Visibility.Collapsed : Visibility.Visible;
+            if (p as string == "!")
+                return (bool) v ? HiddenVisibility : Visibility.Visible;
 
-            return (bool)v ? Visibility.Visible : Visibility.Collapsed;
+            return (bool)v ? Visibility.Visible : HiddenVisibility;
         }
 
         protected override object ConvertBack(object v, Type t, object p, CultureInfo c)
         {
-            if ((p as string) == "!")
+            if (p as string == "!")
                 return (Visibility) v != Visibility.Visible;
 
             return (Visibility)v == Visibility.Visible;
