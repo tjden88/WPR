5cc1ee0d03026afabf59ad8923115d28122f0c2c
Денис <dultsevdv@gmail.com>
2021-10-06 03:21:02 +0500

    Enum2Bool конвертер

---
+++ WPR/Converters/EnumToBoolConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using WPR.MVVM.Converters;
+
+namespace WPR.Converters
+{
+    /// <summary>
+    /// Конвертер enum в bool
+    /// </summary>
+    public class EnumToBoolConverter : ConverterBase
+    {
+        protected override object Convert(object v, Type t, object p, CultureInfo c) => v.Equals(p);
+
+        protected override object ConvertBack(object v, Type t, object p, CultureInfo c) => v.Equals(true) ? p : Binding.DoNothing;
+    }
+}
