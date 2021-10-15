e851a0edc9d5f4b8ca6ef692487f0d3d6be395c8
Денис <dultsevdv@gmail.com>
2021-10-15 21:06:44 +0500

    Измененеие теней в кнопках.
    Добавлена страница команд в тестовом проекте

---
+++ WPR.Demo/Pages/Commands.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using WPR.MVVM.Commands;
+
+namespace WPR.Demo.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Commands.xaml
+    /// </summary>
+    public partial class Commands : Page
+    {
+        public Commands()
+        {
+            InitializeComponent();
+        }
+
+        private int _Count;
+
+        private void ShowBubble(string text) => WPRMessageBox.Bubble(this, text);
+
+        #region Command SimpleCommand - Простая команда
+
+        /// <summary>Простая команда</summary>
+        private Command _SimpleCommand;
+
+        /// <summary>Простая команда</summary>
+        public Command SimpleCommand => _SimpleCommand
+            ??= new Command(OnSimpleCommandExecuted, CanSimpleCommandExecute);
+
+        /// <summary>Проверка возможности выполнения - Простая команда</summary>
+        private bool CanSimpleCommandExecute() => true;
+
+        /// <summary>Логика выполнения - Простая команда</summary>
+        private void OnSimpleCommandExecuted()
+        {
+            ShowBubble("Простая команда");
+            SimpleCommand.Executable = false;
+        }
+
+        #endregion
+
+        #region Command CommandWithTextCommand - Команда с именем
+
+        /// <summary>Команда с именем</summary>
+        private Command _CommandWithTextCommand;
+
+        /// <summary>Команда с именем</summary>
+        public Command CommandWithTextCommand => _CommandWithTextCommand
+            ??= new Command(OnCommandWithTextCommandExecuted, CanCommandWithTextCommandExecute, "Команда с текстом");
+
+        /// <summary>Проверка возможности выполнения - Команда с именем</summary>
+        private bool CanCommandWithTextCommandExecute() => true;
+
+        /// <summary>Логика выполнения - Команда с именем</summary>
+        private void OnCommandWithTextCommandExecuted()
+        {
+            ShowBubble(CommandWithTextCommand.Text);
+            _Count++;
+            CommandWithTextCommand.Text += _Count;
+        }
+
+        #endregion
+
+        #region Command CommandWithKeyGestureCommand - Команда с горячей клавишей
+
+        /// <summary>Команда с горячей клавишей</summary>
+        private Command _CommandWithKeyGestureCommand;
+
+        /// <summary>Команда с горячей клавишей</summary>
+        public Command CommandWithKeyGestureCommand => _CommandWithKeyGestureCommand
+            ??= new Command(OnCommandWithKeyGestureCommandExecuted, CanCommandWithKeyGestureCommandExecute, "Команда с горячей клавишей",
+                new KeyGesture(Key.A, ModifierKeys.Control), this);
+
+        /// <summary>Проверка возможности выполнения - Команда с горячей клавишей</summary>
+        private bool CanCommandWithKeyGestureCommandExecute() => true;
+
+        /// <summary>Логика выполнения - Команда с горячей клавишей</summary>
+        private void OnCommandWithKeyGestureCommandExecuted()
+        {
+            ShowBubble(CommandWithKeyGestureCommand.ToString());
+        }
+
+        #endregion
+
+        #region Command GenericCommand - Типизированная команда
+
+        /// <summary>Типизированная команда</summary>
+        private Command<string> _GenericCommand;
+
+        /// <summary>Типизированная команда</summary>
+        public Command<string> GenericCommand => _GenericCommand
+            ??= new Command<string>(OnGenericCommandExecuted, CanGenericCommandExecute, "Типизированная команда");
+
+        /// <summary>Проверка возможности выполнения - Типизированная команда</summary>
+        private bool CanGenericCommandExecute(string s) => s != string.Empty;
+
+        /// <summary>Логика выполнения - Типизированная команда</summary>
+        private void OnGenericCommandExecuted(string s)
+        {
+            ShowBubble(s);
+            GenericCommand.CanExecuteWithNullParameter = true;
+        }
+
+        #endregion
+    }
+}
--- WPR/Helpers/ButtonHelper.cs
+++ WPR/Helpers/ButtonHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media.Effects;
 
 namespace WPR.Helpers
 {
@@ -11,24 +12,41 @@ namespace WPR.Helpers
         public static readonly DependencyProperty BorderRadiusProperty =
             DependencyProperty.RegisterAttached("BorderRadius", typeof(CornerRadius), typeof(ButtonHelper), new PropertyMetadata(default(CornerRadius)));
 
-        public static void SetBorderRadius(UIElement element, CornerRadius value) => element?.SetValue(BorderRadiusProperty, value);
+        public static void SetBorderRadius(UIElement element, CornerRadius value) => element.SetValue(BorderRadiusProperty, value);
 
-        public static CornerRadius GetBorderRadius(UIElement element) => (CornerRadius)element?.GetValue(BorderRadiusProperty);
+        public static CornerRadius GetBorderRadius(UIElement element) => (CornerRadius)element.GetValue(BorderRadiusProperty);
 
         #endregion
 
-        #region Будет ли обрезаться контент внутри ClippingBorder
+        #region Запретить Ripple выходить за границы кнопки
 
         /// <summary>
-        /// Будет ли обрезаться контент внутри ClippingBorder
+        /// Запретить Ripple выходить за границы кнопки
         /// </summary>
-        public static bool GetBorderClipContent(DependencyObject obj) => (bool)obj?.GetValue(BorderClipContentProperty);
+        public static bool GetNoRippleCircleOutside(DependencyObject obj) => (bool)obj.GetValue(NoRippleCircleOutsideProperty);
 
-        public static void SetBorderClipContent(DependencyObject obj, bool value) => obj?.SetValue(BorderClipContentProperty, value);
+        public static void SetNoRippleCircleOutside(DependencyObject obj, bool value) => obj.SetValue(NoRippleCircleOutsideProperty, value);
 
-        public static readonly DependencyProperty BorderClipContentProperty =
-            DependencyProperty.RegisterAttached("BorderClipContent", typeof(bool), typeof(ButtonHelper), new PropertyMetadata(true));
+        public static readonly DependencyProperty NoRippleCircleOutsideProperty =
+            DependencyProperty.RegisterAttached("NoRippleCircleOutside", typeof(bool), typeof(ButtonHelper), new PropertyMetadata(true));
         #endregion
 
+        #region Attached property ButtonShadow : DropShadowEffect - Тень кнопки
+
+        /// <summary>Тень кнопки</summary>
+        public static readonly DependencyProperty ButtonShadowProperty =
+            DependencyProperty.RegisterAttached(
+                "ButtonShadow",
+                typeof(DropShadowEffect),
+                typeof(ButtonHelper),
+                new PropertyMetadata(default(DropShadowEffect)));
+
+        /// <summary>Тень кнопки</summary>
+        public static void SetButtonShadow(DependencyObject d, DropShadowEffect value) => d.SetValue(ButtonShadowProperty, value);
+
+        /// <summary>Тень кнопки</summary>
+        public static DropShadowEffect GetButtonShadow(DependencyObject d) => (DropShadowEffect) d.GetValue(ButtonShadowProperty);
+
+        #endregion  
     }
 }
