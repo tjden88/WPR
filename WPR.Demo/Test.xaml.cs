13973215900bfe8069bcd99db28f11e7b98f5b26
Денис <dultsevdv@gmail.com>
2021-10-15 17:24:38 +0500

    Удалены тесты директорий

--- WPR.Demo/Test.xaml.cs
+++ WPR.Demo/Test.xaml.cs
@@ -1,39 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
-using Shell32;
-
+﻿
 namespace WPR.Demo
 {
     /// <summary>
     /// Логика взаимодействия для Test.xaml
     /// </summary>
-    public partial class Test : Window
+    public partial class Test
     {
-        public Test()
-        {
-            InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-            List<string> items = new();
-
-            Type shellAppType = Type.GetTypeFromProgID("Shell.Application");
-            if (shellAppType is not null)
-            {
-                object shell = Activator.CreateInstance(shellAppType);
-                Folder2 f2 = (Folder2)shellAppType.InvokeMember("NameSpace",
-                    System.Reflection.BindingFlags.InvokeMethod, null, shell,
-                    new object[] { "shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}" });
-
-                if (f2 != null) items.AddRange(from FolderItem fi in f2.Items() where fi.IsFolder select fi.Path);
-            }
-
-
-            list.ItemsSource = items;
-        }
+        public Test() => InitializeComponent();
     }
 }
--- WPR.Demo/ViewModels/TestViewModel.cs
+++ WPR.Demo/ViewModels/TestViewModel.cs
@@ -1,73 +1,9 @@
-﻿using System.Collections.ObjectModel;
-using System.IO;
-using WPR.Infrastructure.Icons;
-using WPR.MVVM.ViewModels;
+﻿using WPR.MVVM.ViewModels;
 
 namespace WPR.Demo.ViewModels
 {
     class TestViewModel: ViewModel
     {
-        #region Age : int - Тест привязок
 
-        private int _Age;
-
-        /// <summary>Тест привязок</summary>
-        public int Age
-        {
-            get => _Age;
-            set => Set(ref _Age, value);
-        }
-
-        #endregion
-
-        #region RootDirectory : string - Корневые директории
-
-        private ObservableCollection<DirectoryViewModel> _RootDirectories = new();// = new List<DirectoryViewModel>() {new(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile))};
-
-        /// <summary>Корневые директории</summary>
-        public ObservableCollection<DirectoryViewModel> RootDirectories
-        {
-            get => _RootDirectories;
-            set => Set(ref _RootDirectories, value);
-        }
-
-        #endregion
-
-        #region RootIcon : PackIconKind - Иконка корневой папки
-
-        private PackIconKind _RootIcon = PackIconKind.Drive;
-
-        /// <summary>Иконка корневой папки</summary>
-        public PackIconKind RootIcon
-        {
-            get => _RootIcon;
-            set => Set(ref _RootIcon, value);
-        }
-
-        #endregion
-
-        #region SelectedDirectory : DirectoryViewModel - Выбранная папка
-
-        private DirectoryViewModel _SelectedDirectory;
-
-        /// <summary>Выбранная папка</summary>
-        public DirectoryViewModel SelectedDirectory
-        {
-            get => _SelectedDirectory;
-            set => Set(ref _SelectedDirectory, value);
-        }
-
-        #endregion
-
-        public TestViewModel()
-        {
-            foreach (var drive in DriveInfo.GetDrives())
-            {
-                if (drive.DriveType == DriveType.Fixed)
-                {
-                    RootDirectories.Add(new DirectoryViewModel(drive.RootDirectory.FullName));
-                }
-            }
-        }
     }
 }
