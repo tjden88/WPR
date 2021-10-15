605ed74405f2786d03cb68afeda53162996bfb54
Денис <dultsevdv@gmail.com>
2021-10-15 21:25:33 +0500

    Чистка тестового окна от теста команд

--- WPR.Demo/ViewModels/TestViewModel.cs
+++ WPR.Demo/ViewModels/TestViewModel.cs
@@ -5,58 +5,5 @@ namespace WPR.Demo.ViewModels
 {
     class TestViewModel: ViewModel
     {
-        #region Text : string - Текст
-
-        /// <summary>Текст</summary>
-        private string _Text;
-
-        /// <summary>Текст</summary>
-        public string Text
-        {
-            get => _Text;
-            set => Set(ref _Text, value);
-        }
-
-        #endregion
-
-        #region Command TestTypedCommand - Тестовая типизированная команда
-
-        /// <summary>Тестовая типизированная команда</summary>
-        private Command<string> _TestTypedCommand;
-
-        /// <summary>Тестовая типизированная команда</summary>
-        public Command<string> TestTypedCommand => _TestTypedCommand
-            ??= new Command<string>(OnTestTypedCommandExecuted, CanTestTypedCommandExecute){CanExecuteWithNullParameter = true};
-
-        /// <summary>Проверка возможности выполнения - Тестовая типизированная команда</summary>
-        private bool CanTestTypedCommandExecute(string s) => s != "";
-
-        /// <summary>Логика выполнения - Тестовая типизированная команда</summary>
-        private void OnTestTypedCommandExecuted(string s)
-        {
-            WPRMessageBox.ShowModal(null, s);
-        }
-
-        #endregion
-
-        #region Command TestCommand - Тест
-
-        /// <summary>Тест</summary>
-        private Command _TestCommand;
-
-        /// <summary>Тест</summary>
-        public Command TestCommand => _TestCommand
-            ??= new Command(OnTestCommandExecuted, CanTestCommandExecute);
-
-        /// <summary>Проверка возможности выполнения - Тест</summary>
-        private bool CanTestCommandExecute() => true;
-
-        /// <summary>Логика выполнения - Тест</summary>
-        private void OnTestCommandExecuted()
-        {
-            TestTypedCommand.Execute();
-        }
-
-        #endregion
     }
 }
