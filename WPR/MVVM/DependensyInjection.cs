0284f54fcc6f7138e60d2f7fcb7643db0e62ea4a
Денис <dultsevdv@gmail.com>
2021-10-18 23:31:05 +0500

    Сахар в DI

--- WPR/MVVM/DependensyInjection.cs
+++ WPR/MVVM/DependensyInjection.cs
@@ -67,7 +67,7 @@ namespace WPR.MVVM
         /// <typeparam name="TService">Класс сервиса</typeparam>
         /// <param name="parameters">Произвольные параметры конструктора объекта</param>
         /// <returns>Экземпляр класса сервиса или его наследника</returns>
-        public static TService Get<TService>(object[] parameters = null) where TService : class =>
+        public static TService Get<TService>(params object[] parameters) where TService : class =>
             (TService)GetByType(typeof(TService), parameters);
 
 
