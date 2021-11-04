4d528f4000f2d5304fdf517c893a3b94b705f7a1
Денис <dultsevdv@gmail.com>
2021-11-04 16:28:29 +0500

    Изменение многих стилей

--- WPR.Demo/App.xaml.cs
+++ WPR.Demo/App.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System.Windows;
-using WPR.Demo.Services;
-using WPR.Demo.Services.Interfaces;
-using WPR.Demo.ViewModels;
-using WPR.MVVM;
 
 namespace WPR.Demo
 {
@@ -11,17 +7,5 @@ namespace WPR.Demo
     /// </summary>
     public partial class App : Application
     {
-        /// <summary> Получить сервис из контейнера IoC </summary>
-        internal static T GetService<T>() where T: class => DependensyInjection.Get<T>();
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            // Регистрация ViewModel в IoC контейнере
-            DependensyInjection.Registrator.AddSingleton<MainWindowViewModel>()
-                .AddSingleton<TestViewModel>();
-
-            // Регистрация сервисов в IoC контейнере
-            DependensyInjection.Registrator.AddSingleton<IGetPages, GetPages>();
-        }
     }
 }
--- WPR.Demo/ViewModels/Base/VMLocator.cs
+++ WPR.Demo/ViewModels/Base/VMLocator.cs
@@ -1,8 +1,33 @@
-﻿namespace WPR.Demo.ViewModels.Base
+﻿using WPR.Demo.Services;
+
+namespace WPR.Demo.ViewModels.Base
 {
-    internal class VmLocator
+    internal class VMLocator
     {
-        public MainWindowViewModel MainWindowViewModel => App.GetService<MainWindowViewModel>();
-        public TestViewModel TestViewModel => App.GetService<TestViewModel>();
+        #region MainWindowViewModel : MainWindowViewModel - Модель главного окна
+
+        private MainWindowViewModel _MainWindowViewModel;
+
+        /// <summary>Модель главного окна</summary>
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get => _MainWindowViewModel ??= new MainWindowViewModel(new GetPages());
+            set => _MainWindowViewModel = value;
+        }
+
+        #endregion
+
+        #region TestViewModel : TestViewModel - Для тестов вьюмодель
+
+        private TestViewModel _TestViewModel;
+
+        /// <summary>Для тестов вьюмодель</summary>
+        public TestViewModel TestViewModel
+        {
+            get => _TestViewModel ??= new();
+            set => _TestViewModel = value;
+        }
+
+        #endregion
     }
 }
