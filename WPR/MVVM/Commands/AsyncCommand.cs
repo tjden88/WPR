eb12f5778c28d54324e11ce429969e22f7978523
Денис <dultsevdv@gmail.com>
2022-02-28 04:56:49 +0500

    Типизированная асинхронная команда

--- WPR/MVVM/Commands/AsyncCommand.cs
+++ WPR/MVVM/Commands/AsyncCommand.cs
@@ -22,7 +22,7 @@ namespace WPR.MVVM.Commands
         public bool IsNowExecuting
         {
             get => _IsNowExecuting;
-            private set
+            protected set
             {
                 _IsNowExecuting = value;
                 CommandManager.InvalidateRequerySuggested();
@@ -57,8 +57,13 @@ namespace WPR.MVVM.Commands
         #endregion
 
         #region Ctor
+
+        protected AsyncCommand()
+        {
+        }
+
         public AsyncCommand(Func<Task> ExecuteAsync, Func<bool> CanExecute = null, string CommandText = null) :
-    this(ExecuteAsync, CanExecute, CommandText, null, null)
+            this(ExecuteAsync, CanExecute, CommandText, null, null)
         {
         }
 
@@ -85,7 +90,7 @@ namespace WPR.MVVM.Commands
 
             this.ExecuteGesture = ExecuteGesture;
             GestureTarget?.InputBindings.Add(new InputBinding(this, ExecuteGesture));
-        } 
+        }
         #endregion
 
         protected override bool CanExecuteCommand(object P) => !IsNowExecuting && (_CanExecute?.Invoke(P) ?? true);
@@ -99,6 +104,87 @@ namespace WPR.MVVM.Commands
                 IsNowExecuting = true;
 
                 await _ExecuteAsync(P, CancelSource.Token).ConfigureAwait(true);
+            }
+            catch (OperationCanceledException) { }
+            finally
+            {
+                IsNowExecuting = false;
+                CancelSource = null;
+            }
+        }
+    }
+
+
+
+    /// <summary>
+    /// Типизированная асинхронная команда
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class AsyncCommand<T> : AsyncCommand
+    {
+
+        private readonly Func<T, CancellationToken, Task> _ExecuteAsync;
+        private readonly Predicate<T> _CanExecute;
+
+        #region CanExecuteWithNullParameter
+
+        private bool _CanExecuteWithNullParameter;
+
+        /// <summary>Разрешить выполнение команды с параметром = null</summary>
+        public bool CanExecuteWithNullParameter
+        {
+            get => _CanExecuteWithNullParameter;
+            set
+            {
+                if (_CanExecuteWithNullParameter == value) return;
+                _CanExecuteWithNullParameter = value;
+                CommandManager.InvalidateRequerySuggested();
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Ctor
+
+        public AsyncCommand(Func<T, Task> ExecuteAsync, Predicate<T> CanExecute = null, string CommandText = null) :
+            this(ExecuteAsync, CanExecute, CommandText, null, null)
+        {
+        }
+
+        public AsyncCommand(Func<T, Task> ExecuteAsync, Predicate<T> CanExecute, string CommandText, KeyGesture ExecuteGesture, UIElement GestureTarget) :
+            this((o, _) => ExecuteAsync(o), CanExecute, CommandText, ExecuteGesture, GestureTarget)
+        {
+        }
+
+        public AsyncCommand(Func<T, CancellationToken, Task> ExecuteAsync, Predicate<T> CanExecute, string CommandText, KeyGesture ExecuteGesture, UIElement GestureTarget)
+        {
+            _ExecuteAsync = ExecuteAsync;
+            _CanExecute = CanExecute;
+            Text = CommandText;
+
+            this.ExecuteGesture = ExecuteGesture;
+            GestureTarget?.InputBindings.Add(new InputBinding(this, ExecuteGesture));
+        }
+
+        #endregion
+
+
+        protected override bool CanExecuteCommand(object P)
+        {
+            if (!CanExecuteWithNullParameter && P is not T) return false;
+            return _CanExecute?.Invoke((T)P) ?? true;
+        }
+
+        protected override async void ExecuteCommand(object P)
+        {
+            try
+            {
+                CancelSource ??= new();
+
+                IsNowExecuting = true;
+
+                await _ExecuteAsync((T) P, CancelSource.Token).ConfigureAwait(true);
             }
             catch (OperationCanceledException) { }
             finally
