80146139008ccdcd26b435416d06c5c3927f1c6c
Денис (home) <dultsevdv@gmail.com>
2022-12-24 21:31:52 +0500

    Тестовая страница

---
+++ WPR.Demo/Pages/Tests.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using WPR.MVVM.Commands;
+
+namespace WPR.Demo.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Tests.xaml
+    /// </summary>
+    public partial class Tests : Page
+    {
+        public Tests()
+        {
+            InitializeComponent();
+        }
+
+        #region Command TestCommand - Тестовая команда
+
+        /// <summary>Тестовая команда</summary>
+        private Command _TestCommand;
+
+        /// <summary>Тестовая команда</summary>
+        public Command TestCommand => _TestCommand
+            ??= new Command(OnTestCommandExecuted, CanTestCommandExecute, "Тестовая команда", new KeyGesture(Key.A, ModifierKeys.Control), this);
+
+        /// <summary>Проверка возможности выполнения - Тестовая команда</summary>
+        private bool CanTestCommandExecute() => true;
+
+        /// <summary>Логика выполнения - Тестовая команда</summary>
+        private void OnTestCommandExecuted()
+        {
+            WPRMessageBox.Information(this, "asdad");
+        }
+
+        #endregion
+    }
+}
