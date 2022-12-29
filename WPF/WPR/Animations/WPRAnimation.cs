6c167abe31c5f62b005c519dfff05373451cfa5a
Денис (home) <dultsevdv@gmail.com>
2022-12-29 05:24:16 +0500

    Класс-обёртка для анимаций WPF

---
+++ WPF/WPR/Animations/WPRAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace WPR.Animations
+{
+    /// <summary>
+    /// Создание и управление анимациями WPF
+    /// </summary>
+    public class WPRAnimation//<T> where T : DependencyObject
+    {
+        public readonly DependencyObject Target;
+
+        public WPRAnimation(DependencyObject Target)
+        {
+            this.Target = Target;
+            Animation.Completed += (_, _) => OnCompleted?.Invoke();
+        }
+
+
+        public Storyboard Animation { get; set; } = new();
+
+
+        public Action OnCompleted { get; set; }
+
+
+    }
+}
---
+++ WPF/WPR/Animations/WPRAnimationExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace WPR.Animations
+{
+    /// <summary>
+    /// Методы расширения для управления анимациями WPF
+    /// </summary>
+    public static class WPRAnimationExtensions
+    {
+        ///// <summary> Добавить анимацию в коллекцию </summary>
+        //public static WPRAnimation<T> AddChildren<T>(this WPRAnimation<T> a, AnimationTimeline animation) where T : DependencyObject
+        //{ 
+        //    a.Storyboard.Children.Add(animation);
+        //    return a;
+        //}
+
+        /// <summary> Добавить анимацию в коллекцию </summary>
+        public static WPRAnimation AddChildren(this WPRAnimation a, AnimationTimeline animation, string PropertyPath)
+        {
+            Storyboard.SetTarget(animation, a.Target);
+            Storyboard.SetTargetProperty(animation, new PropertyPath(PropertyPath));
+
+            a.Animation.Children.Add(animation);
+            return a;
+        }
+
+
+        /// <summary> Добавить Double анимацию в коллекцию </summary>
+        public static WPRAnimation AddDoubleAnimation(this WPRAnimation a, double From, double To, TimeSpan Duration,
+            string PropertyPath) => AddChildren(a, new DoubleAnimation(From, To, Duration), PropertyPath);
+
+
+        /// <summary> Действие при завершении анимации </summary>
+        public static WPRAnimation OnCompleted(this WPRAnimation a, Action Action)
+        {
+            a.OnCompleted += Action;
+            return a;
+        }
+
+
+        /// <summary> Запустить анимацию </summary>
+        public static void Begin(this WPRAnimation a) => a.Animation.Begin();
+    }
+}
