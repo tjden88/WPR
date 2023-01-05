e57721f91f77c4c33350e2bb7fdeb9137570dc06
Денис (home) <dultsevdv@gmail.com>
2023-01-06 02:04:33 +0500

    Регистратор UI сервисов

---
+++ WPF/WPR.UiServices/Extensions/ServiceRegistrator.cs
@@ -0,0 +1,22 @@
+﻿using WPR.Domain.Interfaces;
+using WPR.UiServices.Interfaces;
+using WPR.UiServices.Themes;
+using WPR.UiServices.UI;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceRegistrator
+{
+
+    /// <summary>
+    /// Добавить сервисы диалогов, навигации, менеджера тем
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddWPRUiServices(this IServiceCollection services) => services
+            .AddSingleton<IUserDialog, UserDialog>()
+            .AddSingleton<IAppNavigation, AppNavigationService>()
+            .AddSingleton<IColorThemeManager, WPRColorThemeManager>()
+    ;
+}
