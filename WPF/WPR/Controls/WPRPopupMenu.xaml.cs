f1be51e0e42c59ba74fe790d38a377bb7cf7cfeb
Денис <dultsevdv@gmail.com>
2023-06-26 00:45:53 +0500

    Меню с попапом

---
+++ WPF/WPR/Controls/WPRPopupMenu.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPR.Controls
+{
+    /// <summary>
+    /// Всплывающее меню (попап с кнопкой)
+    /// </summary>
+    public class WPRPopupMenu : ContentControl
+    {
+        static WPRPopupMenu()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WPRPopupMenu), new FrameworkPropertyMetadata(typeof(WPRPopupMenu)));
+        }
+    }
+}
