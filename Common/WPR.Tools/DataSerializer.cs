2185be305ebd490fce6ffcd5d57fb991db92a75f
Денис <dultsevdv@gmail.com>
2023-06-29 23:17:54 +0500

    Доработка сериализатора

--- Common/WPR.Tools/DataSerializer.cs
+++ Common/WPR.Tools/DataSerializer.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WPR.Tools
 {
@@ -59,6 +61,32 @@ namespace WPR.Tools
             return false;
         }
 
+
+        /// <summary>
+        /// Сохранить в файл
+        /// </summary>
+        /// <param name="obj">Исходный объект</param>
+        /// <param name="FileName">Имя файла</param>
+        /// <param name="cancel">Токен отмены</param>
+        /// <returns>false, если сохранение не удалось</returns>
+        public static async Task<bool> SaveToFileAsync<T>(T obj, string FileName, CancellationToken cancel = default)
+        {
+            try
+            {
+                await using var stream = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.Write);
+                var options = new JsonSerializerOptions { WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping, }; 
+                await JsonSerializer.SerializeAsync(stream, obj, options, cancel);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message, "DataSerializer");
+                LastOperationException = e;
+            }
+            return false;
+        }
+
+
         /// <summary>
         /// Загрузить из файла
         /// </summary>
@@ -72,6 +100,29 @@ namespace WPR.Tools
             {
                 var jsonString = File.ReadAllText(FileName);
                 return JsonSerializer.Deserialize<T>(jsonString);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message, "DataSerializer");
+                LastOperationException = e;
+                return default;
+            }
+        }
+
+        /// <summary>
+        /// Загрузить из файла
+        /// </summary>
+        /// <param name="FileName">Имя файла</param>
+        /// <returns>default, если не удалось</returns>
+        public static async Task<T> LoadFromFileAsync<T>(string FileName, CancellationToken cancel = default)
+        {
+            if (!File.Exists(FileName))
+                return default;
+            try
+            {
+                var options = new JsonSerializerOptions { WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping, };
+                await using var stream = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+                return await JsonSerializer.DeserializeAsync<T>(stream, options, cancel);
             }
             catch (Exception e)
             {
