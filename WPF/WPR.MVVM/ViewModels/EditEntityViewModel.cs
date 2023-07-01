5f9fccb9b7987fc5dba62b59dc87677322f46030
Денис <dultsevdv@gmail.com>
2023-07-02 02:04:18 +0500

    Рефакторинг ВМ редактирования сущности

--- WPF/WPR.MVVM/ViewModels/EditEntityViewModel.cs
+++ WPF/WPR.MVVM/ViewModels/EditEntityViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-using WPR.MVVM.Commands.Base;
+﻿using System.Runtime.CompilerServices;
 
 namespace WPR.MVVM.ViewModels;
 
@@ -9,40 +7,11 @@ namespace WPR.MVVM.ViewModels;
 /// Может запоминать изначальные значения свойств и сбрасывать их.
 /// Проверяет валидацию
 /// </summary>
-/// <typeparam name="TModel">Тип редактируемой сущности</typeparam>
-public abstract class EditEntityViewModel<TModel> : ValidationViewModel
+public abstract class EditEntityViewModel : ValidationViewModel
 {
-    /// <summary>
-    /// Происходит при вызове команд подтверждения или отмены
-    /// </summary>
-    public event Action<bool> Completed; 
-
 
     // словарь для хранения изменённых свойств
-    private readonly Dictionary<string, object> _Properties = new();
-
-
-    /// <summary> Редактируемая сущность </summary>
-    protected TModel Model { get; private set; }
-
-    private readonly Type _ModelType;
-
-
-    protected EditEntityViewModel(bool OnlyForDesignTime) : base(OnlyForDesignTime)
-    {
-        _ModelType = typeof(TModel);
-    }
-
-    protected EditEntityViewModel()
-    {
-        _ModelType = typeof(TModel);
-    }
-
-    protected EditEntityViewModel(TModel Model)
-    {
-        this.Model = Model;
-        _ModelType = typeof(TModel);
-    }
+    protected readonly Dictionary<string, object> Properties = new();
 
 
     /// <summary> Получить свойство вьюмодели </summary>
@@ -50,10 +19,10 @@ public abstract class EditEntityViewModel<TModel> : ValidationViewModel
     {
         if (Property is null) throw new ArgumentNullException(nameof(Property));
 
-        return (T) (_Properties
+        return (T)(Properties
             .TryGetValue(Property, out var value)
             ? value
-            : _ModelType.GetProperty(Property)?.GetValue(Model));
+            : default(T));
     }
 
 
@@ -62,132 +31,107 @@ public abstract class EditEntityViewModel<TModel> : ValidationViewModel
     {
         if (Property is null) throw new ArgumentNullException(nameof(Property));
 
-        if (_Properties.TryGetValue(Property, out var oldValue) && Equals(oldValue, Value))
+        if (Properties.TryGetValue(Property, out var oldValue) && Equals(oldValue, Value))
+            return false;
+
+        Properties[Property] = Value;
+
+        OnPropertyChanged(Property);
+        OnPropertyChanged(nameof(HasChanges));
+        return true;
+    }
+
+    /// <summary> Имеются ли изменённые свойства модели </summary>
+    public bool HasChanges => Properties.Any();
+
+
+    /// <summary> Зафиксировать изменения </summary>
+    public virtual void CommitChanges()
+    {
+        OnCommitChanges();
+
+        Properties.Clear();
+        OnPropertyChanged(nameof(HasChanges));
+    }
+
+
+    /// <summary> Вызывается при фиксации изменений, перед очисткой словаря изменённых свойств </summary>
+    protected abstract void OnCommitChanges();
+
+
+
+    /// <summary> Сбросить изменения </summary>
+    public virtual void RejectChanges()
+    {
+        var changedProperties = Properties.Keys.ToArray();
+        Properties.Clear();
+        OnPropertyChanged(nameof(HasChanges));
+
+        foreach (var property in changedProperties)
+            OnPropertyChanged(property);
+    }
+}
+
+
+/// <summary>
+/// Типизированная модель для редактирования сущности.
+/// Может запоминать изначальные значения свойств и сбрасывать их.
+/// Проверяет валидацию
+/// </summary>
+/// <typeparam name="TModel">Тип редактируемой сущности</typeparam>
+public abstract class EditEntityViewModel<TModel> : EditEntityViewModel
+{
+    protected TModel Model { get; }
+    private readonly Type _ModelType;
+
+    protected EditEntityViewModel(TModel Model)
+    {
+        this.Model = Model;
+        _ModelType = typeof(TModel);
+    }
+
+    
+
+    /// <summary> Получить свойство вьюмодели </summary>
+    protected override T Get<T>([CallerMemberName] string Property = null)
+    {
+        if (Property is null) throw new ArgumentNullException(nameof(Property));
+
+        return (T)(Properties
+            .TryGetValue(Property, out var value)
+            ? value
+            : _ModelType.GetProperty(Property)?.GetValue(Model));
+    }
+
+
+    /// <summary> Установить свойство вьюмодели </summary>
+    protected override bool Set<T>(T Value, [CallerMemberName] string Property = null)
+    {
+        if (Property is null) throw new ArgumentNullException(nameof(Property));
+
+        if (Properties.TryGetValue(Property, out var oldValue) && Equals(oldValue, Value))
             return false;
 
         var defaultValue = _ModelType.GetProperty(Property)?.GetValue(Model);
 
         if (Equals(defaultValue, Value))
-            _Properties.Remove(Property);
+            Properties.Remove(Property);
         else
-            _Properties[Property] = Value;
+            Properties[Property] = Value;
 
         OnPropertyChanged(Property);
+        OnPropertyChanged(nameof(HasChanges));
         return true;
     }
 
-
-    #region HeaderText : string - Заголовок
-
-    /// <summary>Заголовок</summary>
-    private string _HeaderText;
-
-    /// <summary>Заголовок</summary>
-    public string HeaderText
+    protected override void OnCommitChanges()
     {
-        get => _HeaderText;
-        set => Set(ref _HeaderText, value);
-    }
-
-    #endregion
-
-
-    #region Commands
-
-    #region Command CommitCommand - Подтвердить изменения и записать их в редактируемую модель 
-
-    /// <summary>Подтвердить изменения и записать их в редактируемую модель </summary>
-    private Command _CommitCommand;
-
-    /// <summary>Подтвердить изменения и записать их в редактируемую модель </summary>
-    public Command CommitCommand => _CommitCommand
-        ??= new Command(OnCommitCommandExecuted, CanCommitCommandExecute, "Подтвердить изменения и записать их в редактируемую модель ");
-
-    /// <summary>Проверка возможности выполнения - Подтвердить изменения и записать их в редактируемую модель </summary>
-    private bool CanCommitCommandExecute() => !HasErrors && _Properties.Any();
-
-    /// <summary>Логика выполнения - Подтвердить изменения и записать их в редактируемую модель </summary>
-    private void OnCommitCommandExecuted()
-    {
-        ValidateAll();
-        if(HasErrors) return;
-
-        OnCommit();
-        Completed?.Invoke(true);
-    }
-
-    #endregion
-
-
-    #region Command CancelCommand - Отмена редактирования
-
-    /// <summary>Отмена редактирования</summary>
-    private Command _CancelCommand;
-
-    /// <summary>Отмена редактирования</summary>
-    public Command CancelCommand => _CancelCommand
-        ??= new Command(OnCancelCommandExecuted, CanCancelCommandExecute, "Отмена редактирования");
-
-    /// <summary>Проверка возможности выполнения - Отмена редактирования</summary>
-    private bool CanCancelCommandExecute() => true;
-
-    /// <summary>Логика выполнения - Отмена редактирования</summary>
-    private void OnCancelCommandExecuted() => Completed?.Invoke(false);
-
-    #endregion
-
-
-    #region Command RejectCommand - Сбросить изменения
-
-    /// <summary>Сбросить изменения</summary>
-    private Command _RejectCommand;
-
-    /// <summary>Сбросить изменения</summary>
-    public Command RejectCommand => _RejectCommand
-        ??= new Command(OnRejectCommandExecuted, CanRejectCommandExecute, "Сбросить изменения");
-
-    /// <summary>Проверка возможности выполнения - Сбросить изменения</summary>
-    private bool CanRejectCommandExecute() => _Properties.Any();
-
-    /// <summary>Логика выполнения - Сбросить изменения</summary>
-    private void OnRejectCommandExecuted() => OnReject();
-
-    #endregion
-
-    #endregion
-
-
-    /// <summary> Применить изменения </summary>
-    protected virtual void OnCommit()
-    {
-        foreach (var property in _Properties)
+        foreach (var property in Properties)
         {
             var modelProperty = _ModelType.GetProperty(property.Key);
             if (modelProperty is null || !modelProperty.CanWrite) continue;
 
             modelProperty.SetValue(Model, property.Value);
         }
-
-        _Properties.Clear();
-    }
-
-
-    /// <summary> Сбросить изменения </summary>
-    protected virtual void OnReject()
-    {
-        var changedProperties = _Properties.Keys.ToArray();
-        _Properties.Clear();
-
-        foreach (var property in changedProperties)
-            OnPropertyChanged(property);
-    }
-
-
-    /// <summary> Установить новую модель </summary>
-    public virtual void SetNewModel([NotNull] TModel model)
-    {
-        Model = model;
-        _Properties.Clear();
-        OnAllPropertiesChanged();
     }
 }
