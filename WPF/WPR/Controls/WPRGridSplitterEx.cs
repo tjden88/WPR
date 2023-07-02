cf303a52faac60213d930f60e5892f048c0b621f
Денис <dultsevdv@gmail.com>
2023-07-02 17:13:33 +0500

    Логика работы разделителя

--- WPF/WPR/Controls/WPRGridSplitterEx.cs
+++ WPF/WPR/Controls/WPRGridSplitterEx.cs
@@ -8,13 +8,18 @@ using System.Windows.Input;
 namespace WPR.Controls;
 
 /// <summary>
+/// Изменение размера связанного элемента при помощи мыши
 /// </summary>
 public class WPRGridSplitterEx : GridSplitter
 {
-    //static WPRGridSplitterEx()
-    //{
-    //    DefaultStyleKeyProperty.OverrideMetadata(typeof(WPRGridSplitterEx), new FrameworkPropertyMetadata(typeof(WPRGridSplitterEx)));
-    //}
+    /// <summary> Расположения элемента относительно целевого объекта </summary>
+    public enum Placements
+    {
+        Left,
+        Top,
+        Right, 
+        Bottom,
+    }
 
     #region Tagret : FrameworkElement - Цель изменения размера
 
@@ -37,12 +42,13 @@ public class WPRGridSplitterEx : GridSplitter
 
     #endregion
 
-    #region TargetMinWidth : double - Минимальный размер цели
+
+    #region TargetMinSize : double - Минимальный размер цели
 
     /// <summary>Минимальный размер цели</summary>
-    public static readonly DependencyProperty TargetMinWidthProperty =
+    public static readonly DependencyProperty TargetMinSizeProperty =
         DependencyProperty.Register(
-            nameof(TargetMinWidth),
+            nameof(TargetMinSize),
             typeof(double),
             typeof(WPRGridSplitterEx),
             new PropertyMetadata(default(double)));
@@ -50,44 +56,93 @@ public class WPRGridSplitterEx : GridSplitter
     /// <summary>Минимальный размер цели</summary>
     [Category("WPRGridSplitterEx")]
     [Description("Минимальный размер цели")]
-    public double TargetMinWidth
+    public double TargetMinSize
     {
-        get => (double)GetValue(TargetMinWidthProperty);
-        set => SetValue(TargetMinWidthProperty, value);
+        get => (double)GetValue(TargetMinSizeProperty);
+        set => SetValue(TargetMinSizeProperty, value);
     }
 
     #endregion
 
+
+    /// <summary>Расположение элемента относительно целевого объекта</summary>
+    public Placements Placement { get; set; } = Placements.Left;
+
     private double _Delta;
-    private FrameworkElement? _Parent = null;
-    private double _TargetWidth;
+    private FrameworkElement? _Parent;
+    private double _TargetSize;
+    private bool _IsCaptured;
 
     protected override void OnMouseDown(MouseButtonEventArgs e)
     {
         base.OnMouseDown(e);
+        if(e.LeftButton != MouseButtonState.Pressed) return;
+
         _Parent = this.FindVisualParent<Window>();
         if (_Parent == null) return;
 
-        _Delta = e.GetPosition(_Parent).X;
-        _TargetWidth = Tagret.ActualWidth;
-        CaptureMouse();
+        switch (Placement)
+        {
+            case Placements.Left:
+            case Placements.Right:
+                _Delta = e.GetPosition(_Parent).X;
+                _TargetSize = Tagret.ActualWidth;
+                break;
+            case Placements.Top:
+            case Placements.Bottom:
+                _Delta = e.GetPosition(_Parent).Y;
+                _TargetSize = Tagret.ActualHeight;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
+        _IsCaptured = CaptureMouse();
     }
 
     protected override void OnMouseMove(MouseEventArgs e)
     {
         base.OnMouseMove(e);
-        if (!IsMouseCaptured) return;
+        if (!_IsCaptured) return;
 
-        var position = e.GetPosition(_Parent).X;
-        var delta = position - _Delta;
+        double delta;
+        double position;
+        var targetMinSize = TargetMinSize;
+        switch (Placement)
+        {
+            case Placements.Left:
+                position = e.GetPosition(_Parent).X;
+                delta = position - _Delta;
+                
+                Tagret.Width = Math.Max(_TargetSize - delta, targetMinSize);
+                break;
 
-        Tagret.Width = _TargetWidth - delta;
+            case Placements.Top:
+                position = e.GetPosition(_Parent).Y;
+                delta = position - _Delta;
+                Tagret.Height = Math.Max(_TargetSize - delta, targetMinSize);
+                break;
+            case Placements.Right:
+                position = e.GetPosition(_Parent).X;
+                delta = position - _Delta;
+                Tagret.Width = Math.Max(_TargetSize + delta, targetMinSize);
+                break;
 
+            case Placements.Bottom:
+                position = e.GetPosition(_Parent).Y;
+                delta = position - _Delta;
+                Tagret.Height = Math.Max(_TargetSize + delta, targetMinSize);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 
     protected override void OnMouseUp(MouseButtonEventArgs e)
     {
         base.OnMouseUp(e);
         ReleaseMouseCapture();
+        _IsCaptured = false;
+        _Parent = null;
     }
 }
