7ed1a92d8dfdd4157e23f5dade165c0f1021dc5a
Денис <dultsevdv@gmail.com>
2023-07-19 01:55:40 +0500

    Функциональность модели редактирования

--- WPF/WPR.MVVM/ViewModels/EditEntityViewModel.cs
+++ WPF/WPR.MVVM/ViewModels/EditEntityViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Media3D;
 
 namespace WPR.MVVM.ViewModels;
 
@@ -81,20 +85,75 @@ public abstract class EditEntityViewModel : ValidationViewModel
 /// <typeparam name="TModel">Тип редактируемой сущности</typeparam>
 public abstract class EditEntityViewModel<TModel> : EditEntityViewModel
 {
-    protected TModel Model { get; }
+    protected TModel Model { get; private set; }
     private readonly Type _ModelType;
 
-    protected EditEntityViewModel(TModel Model)
+    private readonly Dictionary<string, PropertyInfo> _IncludedProperties = new(); // Свойства с аттрибутом IncludedPropertyAttribute: имя собственного свойства, свойство модели
+
+    protected EditEntityViewModel(TModel Model) : this()
     {
         this.Model = Model;
-        _ModelType = typeof(TModel);
     }
 
-    
+    protected EditEntityViewModel()
+    {
+        _ModelType = typeof(TModel);
+        foreach (var property in GetType().GetProperties())
+        {
+            var attr = property.GetCustomAttributes(typeof(IncludedPropertyAttribute), true);
+            if (attr.Length > 0)
+            {
+                var attribute = (IncludedPropertyAttribute)attr[0];
+                var modelPropertyName = attribute.ModelPropertyName ?? property.Name;
+                _IncludedProperties.Add(property.Name, _ModelType.GetProperty(modelPropertyName));
+            }
+        }
+    }
+
+    protected override void OnPropertyChanged(string PropertyName = null)
+    {
+        if (_IncludedProperties.TryGetValue(PropertyName!, out var modelProperty))
+        {
+            var property = GetType().GetProperty(PropertyName);
+
+            var defaultValue = modelProperty?.GetValue(Model);
+            var currentValue = property!.GetValue(this);
+
+            if (Equals(defaultValue, currentValue))
+                Properties.Remove(modelProperty!.Name);
+            else
+                Properties[modelProperty!.Name] = currentValue;
+
+            base.OnPropertyChanged(PropertyName);
+            OnPropertyChanged(nameof(HasChanges));
+            return;
+        }
+        base.OnPropertyChanged(PropertyName);
+    }
+
+
+    /// <summary>
+    /// Установить модель и начать отслеживание свойств
+    /// </summary>
+    /// <param name="model">Экземпляр модели</param>
+    public virtual void SetModel(TModel model)
+    {
+        Properties.Clear();
+        Model = model;
+        foreach (var (key, value) in _IncludedProperties)
+        {
+            var prop = GetType().GetProperty(key);
+            if (prop is null || !prop.CanWrite) continue;
+            prop.SetValue(this, value?.GetValue(model));
+        }
+    }
+
+
 
     /// <summary> Получить свойство вьюмодели </summary>
     protected override T Get<T>([CallerMemberName] string Property = null)
     {
+        if (Model == null) return default;
         if (Property is null) throw new ArgumentNullException(nameof(Property));
 
         return (T)(Properties
@@ -133,5 +192,31 @@ public abstract class EditEntityViewModel<TModel> : EditEntityViewModel
 
             modelProperty.SetValue(Model, property.Value);
         }
+
+        foreach (var (key, value) in _IncludedProperties)
+        {
+            var prop = GetType().GetProperty(key);
+            value.SetValue(Model, prop?.GetValue(this));
+        }
+    }
+
+
+
+    /// <summary> 
+    /// Данное свойство будет отслеживаться и учитываться при вызове методов SetModel и CommitChanges
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    protected class IncludedPropertyAttribute : Attribute
+    {
+        public IncludedPropertyAttribute(string ModelPropertyName = null)
+        {
+            this.ModelPropertyName = ModelPropertyName;
+        }
+
+        /// <summary>
+        /// Имя свойства модели
+        /// </summary>
+        [MaybeNull]
+        public string ModelPropertyName { get; init; }
     }
 }
