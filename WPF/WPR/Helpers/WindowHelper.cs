95964800451c86cfab92a10949ab507938c58432
Денис <dultsevdv@gmail.com>
2024-02-19 22:49:36 +0500

    Добавление свойства цвета шрифта кнопок окна

--- WPF/WPR/Helpers/WindowHelper.cs
+++ WPF/WPR/Helpers/WindowHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 
 namespace WPR.Helpers;
 
@@ -20,5 +21,24 @@ public static class WindowHelper
     public static readonly DependencyProperty WindowHeaderContentProperty =
         DependencyProperty.RegisterAttached("WindowHeaderContent", typeof(object), typeof(WindowHelper), new PropertyMetadata(null));
     #endregion
+
+
+    #region Attached property HeaderButtonsForeground : Brush - Цвет служебных кнопок в заголовке
+
+    /// <summary>Цвет служебных кнопок в заголовке</summary>
+    public static readonly DependencyProperty HeaderButtonsForegroundProperty =
+        DependencyProperty.RegisterAttached(
+            "HeaderButtonsForeground",
+            typeof(Brush),
+            typeof(WindowHelper),
+            new PropertyMetadata(default(Brush)));
+
+    /// <summary>Цвет служебных кнопок в заголовке</summary>
+    public static void SetHeaderButtonsForeground(DependencyObject d, Brush value) => d.SetValue(HeaderButtonsForegroundProperty, value);
+
+    /// <summary>Цвет служебных кнопок в заголовке</summary>
+    public static Brush GetHeaderButtonsForeground(DependencyObject d) => (Brush) d.GetValue(HeaderButtonsForegroundProperty);
+
+    #endregion  
 
 }
