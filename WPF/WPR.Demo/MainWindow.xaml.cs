c28f191a61b4df9bbc5ac58a4bd7a76f44c15f93
Денис <dultsevdv@gmail.com>
2025-03-28 08:37:17 +0500

    Анимация страниц

--- WPF/WPR.Demo/MainWindow.xaml.cs
+++ WPF/WPR.Demo/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media.Animation;
+using WPR.Animations;
 
 namespace WPR.Demo
 {
@@ -10,6 +13,17 @@ namespace WPR.Demo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void PagesFrame_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            if(!PagesFrame.IsLoaded)return;
+            new Storyboard()
+                .AddDoubleAnimation("RenderTransform.Y", 30, 0, 250, EasingFunctions.ExponentialEaseOut)
+                .AddDoubleAnimation("Opacity")
+                .ClearOnComplete()
+                .Start(PagesFrame);
         }
     }
 }
