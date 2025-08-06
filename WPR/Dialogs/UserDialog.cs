f9632793fd75ceea400c16a472934d0d5983f91d
Denis <Denis@HOME-PC>
2025-08-06 22:47:37 +0500

    Начало рефакторинга UserDialog

---
+++ WPR/Dialogs/UserDialog.cs
@@ -0,0 +1,221 @@
+﻿using System.Windows;
+using WPR.Theme;
+
+namespace WPR.Dialogs;
+
+public class WPRUserDialog : IUserDialog
+{
+
+    private static Window? Active => Application.Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsActive);
+
+
+    //public async Task InformationAsync(string message, string? Title = null) => 
+    //    await DoDispatcheredAction(WPRDialogHelper.InformationAsync(Active, message, Title));
+
+
+    //public async Task<bool> QuestionAsync(string message, string? Title = null) =>
+    //    await GetDispatcheredResult(() => WPRDialogHelper.QuestionAsync(Active, message, Title));
+
+
+    //public async Task<bool?> QuestionAsync(string message, IUserDialog.DialogTypes DilaogType, string? Title = null) =>
+    //    DilaogType switch
+    //    {
+    //        IUserDialog.DialogTypes.YesNo => await GetDispatcheredResult(() => WPRDialogHelper.QuestionAsync(Active, message, Title)),
+    //        IUserDialog.DialogTypes.YesNoCancel => await GetDispatcheredResult(() => WPRDialogHelper.QuestionCancelAsync(Active, message, Title)),
+    //        IUserDialog.DialogTypes.OkCancel => await GetDispatcheredResult(() => WPRDialogHelper.InformationCancelAsync(Active, message, Title)),
+    //        _ => throw new ArgumentOutOfRangeException(nameof(DilaogType), DilaogType, "Неверный тип диалога")
+    //    };
+
+
+
+    //public async Task<bool?> CustomQuestionAsync(string message, string? Title, string TrueCaption, string? FalseCaption = null,
+    //    string? NullCaption = null) =>
+    //    await GetDispatcheredResult(() => WPRDialogHelper
+    //        .ShowCustomButtonsDialog(Active, message, Title, TrueCaption, FalseCaption, NullCaption));
+
+
+
+    //public async Task ErrorMessageAsync(string message, string? Title = "Ошибка") =>
+    //    await DoDispatcheredAction(WPRDialogHelper.ErrorAsync(Active, message, Title));
+
+
+    //public async Task<bool> CustomDialogAsync(IWPRDialog Dialog)
+    //    => await GetDispatcheredResult(() => WPRDialogHelper.ShowCustomDialogAsync(Active, Dialog));
+
+
+    //public Task<string?> InputValidatedTextAsync(Action<InputDialogFilterOptions> options)
+    //{
+    //    var filterOptions = new InputDialogFilterOptions();
+    //    options.Invoke(filterOptions);
+    //    return InputValidatedTextAsync(filterOptions);
+    //}
+
+
+    //public async Task<string?> InputTextAsync(string title, string? DefaultValue = null, string? message = null, bool MultiLine = false) =>
+    //    await GetDispatcheredResult(() => WPRDialogHelper.InputTextAsync(Active, title, message, DefaultValue, MultiLine));
+
+    //private async Task<string?> InputValidatedTextAsync(InputDialogFilterOptions DialogFilter) =>
+    //    await GetDispatcheredResult(() => WPRDialogHelper.InputTextAsync(Active,
+    //        DialogFilter.Title,
+    //        DialogFilter.Message,
+    //        DialogFilter.DefaultValue,
+    //        DialogFilter.Validation.Select(f => new PredicateValidationRule<string>(f.Rule, f.ErrorMessage)),
+    //        DialogFilter.MultiLine));
+
+    //public Task ShowNotificationAsync(string message, int delay = 2000, StyleBrushes Backgound = StyleBrushes.BackgroundContrastColorBrush)
+    //{
+    //    Application.Current.Dispatcher.Invoke(() => WPRDialogHelper.Bubble(Active, message, delay, Backgound));
+    //    return Task.CompletedTask;
+    //}
+
+    //public async Task<bool> ShowQuestionNotificationAsync(string message, string AcceptCaption, int delay = 3000, StyleBrushes Backgound = StyleBrushes.BackgroundContrastColorBrush)
+    //{
+    //    return await GetDispatcheredResult(() =>
+    //    {
+    //        var result = new TaskCompletionSource<bool>();
+    //        WPRDialogHelper.Bubble(Active, message, AcceptCaption, b => result.TrySetResult(b), delay, Backgound);
+    //        return result.Task;
+    //    });
+    //}
+
+    //public Task<string?> ShowOpenFileDialogAsync(string Title, IEnumerable<FileFilter>? Filters = null, string InitFileName = "")
+    //{
+    //    var ofd = new OpenFileDialog()
+    //    {
+    //        FileName = InitFileName,
+    //        Title = Title
+    //    };
+
+    //    if (Filters != null)
+    //    {
+    //        var ofdFilter = Filters
+    //            .Select(f => $"{f.Description}|{string.Concat(f.FileMathPatterns.Select(e => $"{e};"))}");
+    //        ofd.Filter = string.Join("|", ofdFilter);
+    //    }
+
+    //    Window? window = null;
+    //    Application.Current.Dispatcher.Invoke(() => window = Active);
+
+    //    var dialogResult = ofd.ShowDialog(window);
+
+    //    return Task.FromResult(dialogResult == true ? ofd.FileName : null);
+    //}
+
+    //public Task<string?> ShowSaveFileDialogAsync(string Title, IEnumerable<FileFilter>? Filters = null, string InitFileName = "")
+    //{
+    //    var sfd = new SaveFileDialog
+    //    {
+    //        FileName = InitFileName,
+    //        Title = Title
+    //    };
+
+    //    if (Filters != null)
+    //    {
+    //        var ofdFilter = Filters
+    //            .Select(f => $"{f.Description}|{string.Concat(f.FileMathPatterns.Select(e => $"{e};"))}");
+    //        sfd.Filter = string.Join("|", ofdFilter);
+    //    }
+
+    //    Window? window = null;
+    //    Application.Current.Dispatcher.Invoke(() => window = Active);
+
+    //    var dialogResult = sfd.ShowDialog(window);
+
+    //    return Task.FromResult(dialogResult == true ? sfd.FileName : null);
+    //}
+
+    //public Task<string?> ShowFolderSelectDialogAsync(string Title, string InitPathName = "")
+    //{
+    //    var dialog = new OpenFolderDialog()
+    //    {
+    //        DefaultDirectory = InitPathName,
+    //        Title = Title
+            
+    //    };
+
+    //    if (!dialog.ShowDialog(Active) == true)
+    //        return Task.FromResult<string?>(null);
+
+    //    return Task.FromResult(dialog.FolderName)!;
+    //}
+
+
+    //private static async Task DoDispatcheredAction(Task action) => await Application.Current.Dispatcher.Invoke(async () => await action, DispatcherPriority.Normal);
+
+
+    //private static async Task<T?> GetDispatcheredResult<T>(Func<Task<T>> action)
+    //{
+    //    T? result = default;
+
+    //    await Application.Current.Dispatcher.Invoke(async () => result = await action.Invoke(), DispatcherPriority.Normal);
+
+    //    return result;
+    //}
+    public async Task InformationAsync(string message, string Title = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> QuestionAsync(string message, string Title = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool?> QuestionAsync(string message, DialogType dialogType, string Title = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool?> CustomQuestionAsync(string message, string Title, string AcceptCaption, string RejectCaption = null,
+        string CancelCaption = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task ErrorMessageAsync(string message, string Title = "Ошибка")
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> CustomDialogAsync(IWPRDialog Dialog)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> InputTextAsync(string title, string DefaultValue = null, string message = null, bool MultiLine = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> InputValidatedTextAsync(Action<InputDialogFilterOptions> options)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task ShowNotificationAsync(string message, StyleBrushes background = StyleBrushes.BackgroundContrastColorBrush,
+        int delay = 2000)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> ShowQuestionNotificationAsync(string message, string AcceptCaption,
+        StyleBrushes background = StyleBrushes.BackgroundContrastColorBrush, int delay = 3000)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> ShowOpenFileDialogAsync(string Title, IEnumerable<FileFilter> Filters = null, string InitFileName = "")
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> ShowSaveFileDialogAsync(string Title, IEnumerable<FileFilter> Filters = null, string InitFileName = "")
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> ShowFolderSelectDialogAsync(string Title, string InitPathName = "")
+    {
+        throw new NotImplementedException();
+    }
+}
--- WPR/Dialogs/WPRDialogHelper.cs
+++ WPR/Dialogs/WPRDialogHelper.cs
@@ -1,16 +1,14 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 using WPR.Infrastructure.Extensions;
-using WPR.Theme;
 
 namespace WPR.Dialogs;
 
 /// <summary>Диалоговые окна</summary>
-public static class WPRDialogHelper
+internal static class UserDialogHelper
 {
     private static readonly Style _ModalWindowStyle = (Style) Application.Current.Resources["WPRModalWindow"];
 
-    #region Private
     // Найти панель для отображения диалога
     [return: MaybeNull]
     private static WPRDialogPanel FindDialogPanel(DependencyObject uIElement)
@@ -24,442 +22,38 @@ public static class WPRDialogHelper
         return uIElement.FindVisualParent<WPRDialogPanel>();
     }
 
-    private static void Show(DependencyObject sender, string Caption, string Title, Action<bool?> Callback, bool CancelButton, bool YesNoButtons, bool isError = false)
-    {
-        // Ищем панель
-        var panel = FindDialogPanel(sender);
-        if (panel == null)
-        {
-            //throw new ArgumentNullException(nameof(panel), "Окно с панелью диалога не найдено!");
-            var showModal = ShowModal(sender, Caption, Title, CancelButton, YesNoButtons);
-            Callback?.Invoke(showModal);
-            return;
-        }
 
-        MessageDialog messageBox = new()
-        {
-            Title = Title,
-            Caption = Caption,
-            CancelButtonVisible = CancelButton,
-            YesNoButtonsVisible = YesNoButtons,
-            IsErrorMessage = isError,
-        };
-        // При клике по кнопке мессаджа закрыть окно и вернуть прозрачность как была
-        messageBox.DialogResult += (b) =>
-        {
-            panel.Hide();
-            Callback?.Invoke(b);
-        };
-        panel.Show(messageBox, true);
-    }
-
-    #endregion
-
-    #region ShowDialog
-
-    /// <summary>
-    /// Показать как модальное окно
-    /// </summary>
-    /// <param name="sender">Объект, окно которого будет розительским по отношению к диалогу.
-    /// Если null, диалог будет поверх всех окон</param>
-    /// <param name="Caption">Сообщение</param>
-    /// <param name="Title">Заголовок</param>
-    /// <param name="CancelButton">Показать кнопку отмены</param>
-    /// <param name="YesNoButtons">Показать кнопки Да/Нет вместо ОК</param>
-    /// <returns></returns>
-    public static bool? ShowModal(DependencyObject sender, string Caption, string Title = "", bool CancelButton = false, bool YesNoButtons = false)
-    {
-        bool? result = null;
-
-        MessageDialog messageBox = new()
-        {
-            Title = Title,
-            Caption = Caption,
-            CancelButtonVisible = CancelButton,
-            YesNoButtonsVisible = YesNoButtons
-        };
-
-        var owner = sender as Window ?? sender?.FindVisualParent<Window>();
-
-        var panel = FindDialogPanel(owner);
-
-        Window dlg = new()
-        {
-            WindowStartupLocation = WindowStartupLocation.CenterOwner,
-            Owner = owner,
-            Topmost = owner == null,
-            Content = messageBox,
-            Style = _ModalWindowStyle
-        };
-
-        messageBox.DialogResult += B =>
-        {
-            result = B;
-            dlg.Close();
-        };
-
-        panel?.Show(null,true);
-
-        dlg.ShowDialog();
-
-        panel?.Hide();
-
-        return result;
-    }
-
-
-    /// <summary>
-    /// Показать кастомный диалог в модальном окне
-    /// </summary>
-    /// <param name="sender">Объект, окно которого будет розительским по отношению к диалогу.
-    /// Если null, диалог будет поверх всех окон</param>
-    /// <param name="Content">Диалог</param>
-    /// <returns></returns>
-    public static bool ShowCustomModal(DependencyObject sender, IWPRDialog Content)
-    {
-        var result = false;
-
-        var owner = sender as Window ?? sender?.FindVisualParent<Window>();
-
-        var panel = FindDialogPanel(owner);
-
-        Window dlg = new()
-        {
-            WindowStartupLocation = WindowStartupLocation.CenterOwner,
-            Owner = owner,
-            Topmost = owner == null,
-            Content = Content.DialogContent,
-            Style = _ModalWindowStyle
-        };
-
-        Content.Completed += B =>
-        {
-            result = B;
-            dlg.Close();
-        };
-
-        panel?.Show(null, true);
-
-        dlg.ShowDialog();
-
-        panel?.Hide();
-
-        return result;
-    }
-    #endregion
-
-    #region Information
-    /// <summary>Показать информационное окно</summary>
-    public static void Information(DependencyObject sender, string Caption, Action Callback = null)
-    {
-        Information(sender, Caption, string.Empty, Callback);
-    }
-
-    /// <summary>Показать информационное окно</summary>
-    public static void Information(DependencyObject sender, string Caption, string Title, Action Callback = null)
-    {
-        Show(sender, Caption,Title, B => Callback?.Invoke(), false, false);
-    }
-    #endregion
-
-    #region InformationAsync
-    /// <summary>Показать информационное окно</summary>
-    public static async Task InformationAsync(DependencyObject sender, string Caption)
-    {
-        await InformationAsync(sender, Caption, string.Empty).ConfigureAwait(false);
-    }
-
-    /// <summary>Показать информационное окно</summary>
-    public static async Task InformationAsync(DependencyObject sender, string Caption, string Title)
-    {
-        TaskCompletionSource complete = new();
-
-        Information(sender, Caption, Title, () => complete.TrySetResult());
-        await complete.Task.ConfigureAwait(false);
-    }
-    #endregion
-
-    #region InformationCancel
-
-    /// <summary>Показать диалог с кнопками ОК, ОТМЕНА</summary>
-    public static void InformationCancel(DependencyObject sender, string Caption, Action<bool> Callback = null)
-    {
-        InformationCancel(sender, Caption, string.Empty, Callback);
-    }
-    /// <summary>Показать диалог с кнопками ОК, ОТМЕНА</summary>
-    public static void InformationCancel(DependencyObject sender, string Caption, string Title, Action<bool> Callback = null)
-    {
-        Show(sender, Caption, Title, B => Callback?.Invoke(B ?? false), true, false);
-    }
-    #endregion
-
-    #region InformationCancelAsync
-
-    /// <summary>Показать диалог с кнопками ОК, ОТМЕНА</summary>
-    public static async Task<bool> InformationCancelAsync(DependencyObject sender, string Caption)
-    {
-        return await InformationCancelAsync(sender, Caption, string.Empty);
-    }
-    /// <summary>Показать диалог с кнопками ОК, ОТМЕНА</summary>
-    public static async Task<bool> InformationCancelAsync(DependencyObject sender, string Caption, string Title)
-    {
-        TaskCompletionSource<bool> complete = new();
-        InformationCancel(sender, Caption, Title, (b) => complete.TrySetResult(b));
-        return await complete.Task.ConfigureAwait(false);
-    }
-    #endregion
-
-    #region Question
-    /// <summary>Показать диалог с кнопками ДА, НЕТ</summary>
-    public static void Question(DependencyObject sender, string Caption, Action<bool> Callback = null)
-    {
-        Question(sender, Caption, string.Empty, Callback);
-    }
-
-    /// <summary>Показать диалог с кнопками ДА, НЕТ</summary>
-    public static void Question(DependencyObject sender, string Caption, string Title, Action<bool> Callback = null)
-    {
-        Show(sender, Caption, Title, B => Callback?.Invoke(B ?? false), false, true);
-    }
-    #endregion
-
-    #region QuestionAsync
-    /// <summary>Показать диалог с кнопками ДА, НЕТ</summary>
-    public static async Task<bool> QuestionAsync(DependencyObject sender, string Caption)
-    {
-        return await QuestionAsync(sender, Caption, string.Empty);
-    }
-    /// <summary>Показать диалог с кнопками ДА, НЕТ</summary>
-    public static async Task<bool> QuestionAsync(DependencyObject sender, string Caption, string Title)
-    {
-        TaskCompletionSource<bool> complete = new();
-        Question(sender, Caption, Title, (b) => complete.TrySetResult(b));
-        return await complete.Task.ConfigureAwait(false);
-    }
-
-    #endregion
-
-    #region QuestionCancel
-    /// <summary>Показать диалог с кнопками ДА, НЕТ, ОТМЕНА</summary>
-    public static void QuestionCancel(DependencyObject sender, string Caption, Action<bool?> Callback = null)
-    {
-        QuestionCancel(sender, Caption, string.Empty, Callback);
-    }
-
-    /// <summary>Показать диалог с кнопками ДА, НЕТ, ОТМЕНА</summary>
-    public static void QuestionCancel(DependencyObject sender, string Caption, string Title, Action<bool?> Callback = null)
-    {
-        Show(sender, Caption, Title, Callback, true, true);
-    }
-    #endregion
-
-    #region QuestionCancelAsync
-
-    /// <summary>Показать диалог с кнопками ДА, НЕТ, ОТМЕНА</summary>
-    public static async Task<bool?> QuestionCancelAsync(DependencyObject sender, string Caption)
-    {
-        return await QuestionCancelAsync(sender, Caption, string.Empty);
-    }
-    /// <summary>Показать диалог с кнопками ДА, НЕТ, ОТМЕНА</summary>
-    public static async Task<bool?> QuestionCancelAsync(DependencyObject sender, string Caption, string Title)
-    {
-        TaskCompletionSource<bool?> complete = new();
-        QuestionCancel(sender, Caption, Title, (b) => complete.TrySetResult(b));
-        return await complete.Task.ConfigureAwait(false);
-    }
-    #endregion
-
-    #region Error
-
-    /// <summary>Показать информационное окно с сообщением об ошибке</summary>
-    public static void Error(DependencyObject sender, string ErrorMessage, string Title = "Ошибка", Action Callback = null)
-    {
-        Show(sender, ErrorMessage, Title, B => Callback?.Invoke(), false, false, true);
-    }
-    #endregion
-
-    #region ErrorAsync
-
-    /// <summary>Показать информационное окно с сообщением об ошибке</summary>
-    public static async Task ErrorAsync(DependencyObject sender, string ErrorMessage, string Title = "Ошибка")
-    {
-        TaskCompletionSource complete = new();
-
-        Error(sender, ErrorMessage, Title, () => complete.TrySetResult());
-        await complete.Task.ConfigureAwait(false);
-    }
-    #endregion
-
-    #region Bubble
-
-    /// <summary>Показать всплывающее сообщение</summary>
-    public static void Bubble(DependencyObject sender, string Text, int Duration = 3000, StyleBrushes Background = StyleBrushes.BackgroundContrastColorBrush)
-    {
-        var p = FindDialogPanel(sender);
-        if(p == null)
-            Show(sender, Text, null, null, false, false);
-        else
-            p.ShowBubble(Text, Duration, BubbleBackground: Background);
-    }
-
-    /// <summary>Показать всплывающее сообщение с кнопкой</summary>
-    public static void Bubble(DependencyObject sender, string Text, string ButtonText, Action<bool> Callback, int Duration = 4000, StyleBrushes Background = StyleBrushes.BackgroundContrastColorBrush)
-    {
-        var p = FindDialogPanel(sender);
-        if (p == null)
-            Show(sender, Text, null, res => Callback?.Invoke(res == true), false, false);
-        else
-            p.ShowBubble(Text, Duration, ButtonText, Callback, Background);
-    }
-
-    #endregion
-
-    #region InputBoxes
-
-    public static void InputText(DependencyObject sender, string Title, Action<bool, string> Callback, string Caption, string DefaultValue, bool MultiLine)
-    {
-        InputText(sender, Title, Callback, Caption, DefaultValue, MultiLine, S => true);
-    }
-
-    public static void InputText(DependencyObject sender, string Title, Action<bool, string> Callback, string Caption, string DefaultValue, bool MultiLine, Predicate<string> ValidationRule , string ValidationErrorMessage = "Неверное значение")
-    {
-        var rule = new PredicateValidationRule<string>(ValidationRule, ValidationErrorMessage);
-
-        InputText(sender, Title, Callback, Caption, DefaultValue, new[] {rule}, MultiLine);
-    }
-
-    public static void InputText(DependencyObject sender, string Title, Action<bool, string> Callback, string Caption, string DefaultValue, IEnumerable<PredicateValidationRule<string>> ValidationRules, bool MultiLine)
-    {
-        // Ищем панель
-        var panel = FindDialogPanel(sender);
-
-        InputDialog inputDialog = new(ValidationRules, DefaultValue, MultiLine)
-        {
-            Title = Title,
-            Caption = Caption,
-        };
-
-
-        if (panel is null)
-        {
-
-            var dlg = new WprInputDialog(inputDialog);
-            inputDialog.DialogResult += b => dlg.CallComplete(b == true);
-
-            var result = ShowCustomModal(sender, dlg);
-            Callback?.Invoke(result, inputDialog.TextValue);
-            return;
-        }
-
-            // При клике по кнопке мессаджа закрыть окно и вернуть прозрачность как была
-        inputDialog.DialogResult += b =>
-        {
-            panel.Hide();
-            Callback?.Invoke(b == true, inputDialog.TextValue);
-        };
-        panel.Show(inputDialog, true);
-    }
-
-
-    private class WprInputDialog(InputDialog Dialog) : IWPRDialog
-    {
-        public object DialogContent { get; } = Dialog;
-
-        public event Action<bool> Completed;
-
-        public void CallComplete(bool result) => Completed?.Invoke(result);
-    }
-
-
-    /// <summary> Поле ввода текста </summary>
-    public static Task<string> InputTextAsync(DependencyObject sender, string Title, string Caption, string DefaultValue, bool MultiLine) => 
-        InputTextAsync(sender, Title, Caption, DefaultValue, S => true, MultiLine);
-
-
-    /// <summary> Поле ввода текста с валидацией </summary>
-    public static Task<string> InputTextAsync(DependencyObject sender, string Title, string Caption, string DefaultValue, Predicate<string> ValidationRule, bool MultiLine, string ValidationErrorMessage = "Неверное значение") => 
-        InputTextAsync(sender, Title, Caption , DefaultValue, new []{new PredicateValidationRule<string>(ValidationRule, ValidationErrorMessage) }, MultiLine);
-
-
-    /// <summary> Поле ввода текста с коллекцией валидаций </summary>
-    public static async Task<string> InputTextAsync(DependencyObject sender, string Title, string Caption, string DefaultValue, IEnumerable<PredicateValidationRule<string>> ValidationRules, bool MultiLine)
-    {
-        TaskCompletionSource<string> complete = new();
-        InputText(sender, Title, (B, S) => { complete.TrySetResult(B ? S : null); }, Caption, DefaultValue, ValidationRules, MultiLine);
-        return await complete.Task.ConfigureAwait(false);
-    }
-
-    #endregion
-
-    #region CustomButtons
-
-    public static async Task<bool?> ShowCustomButtonsDialog(DependencyObject sender, string Caption, string Title, string AcceptButtonText,
-        string DeclineButtonText, string CancelButtonText)
-    {
-        var dialog = new CustomButtonsDialog
-        {
-            Title = Title,
-            AcceptButtonText = AcceptButtonText,
-            DeclineButtonText = DeclineButtonText,
-            CancelButtonText = CancelButtonText,
-            Caption = Caption,
-        };
-
-        await ShowCustomDialogAsync(sender, dialog);
-
-        return dialog.DialogBaseResult;
-    }
-
-    #endregion
-
-    #region CustomDialog
-
-    /// <summary>Показать диалог с кастомным содержимым</summary>
-    public static void ShowCustomDialog(DependencyObject sender, IWPRDialog Content, Action<bool> Callback = null)
+    private static Task<IWPRDialog> Show(DependencyObject sender, IWPRDialog dialog)
     {
         // Ищем панель
         var panel = FindDialogPanel(sender);
 
         if (panel is null)
         {
-            var modal = ShowCustomModal(sender, Content);
-            Callback?.Invoke(modal);
-            return;
+           // Modal
+            return Task.FromResult(dialog);
         }
 
-        Content.Completed += b =>
+        var complete = new TaskCompletionSource<IWPRDialog>();
+
+        dialog.Completed += b =>
         {
             panel.Hide();
-            Callback?.Invoke(b);
+            complete.TrySetResult(dialog);
         };
-        panel.Show(Content, Content.StaysOpen);
+        panel.Show(dialog, dialog.StaysOpen);
+
+        return complete.Task;
     }
 
-    /// <summary>Показать диалог с кастомным содержимым</summary>
-    public static async Task<bool> ShowCustomDialogAsync(DependencyObject sender, IWPRDialog Content)
-    {
-        TaskCompletionSource<bool> complete = new();
-        ShowCustomDialog(sender, Content, (b) => complete.TrySetResult(b));
-        return await complete.Task.ConfigureAwait(false);
-    }
 
-    #endregion
 }
 
 public static class UserDialog
 {
-    private static readonly Style _ModalWindowStyle = (Style)Application.Current.Resources["WPRModalWindow"];
 
-    // Найти панель для отображения диалога
-    [return: MaybeNull]
-    private static WPRDialogPanel FindDialogPanel(DependencyObject uIElement)
-    {
-        if (uIElement == null)
-            return null;
-
-        if (uIElement is Window window)
-            return window.Template?.FindName("WindowDialogPanel", window) as WPRDialogPanel;
-
-        return uIElement.FindVisualParent<WPRDialogPanel>();
-    }
+    /// <summary>
+    /// Экземпляр диалога, который будет использоваться в приложении.
+    /// </summary>
+    public static IUserDialog Default { get; } = new WPRUserDialog();
 }
