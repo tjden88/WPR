90e7351dfbf51c7ca630ecc2a0b725feeea878fc
Denis <Denis@HOME-PC>
2025-08-07 05:16:55 +0500

    Базовая вьюмодель

---
+++ WPR.Mvvm/ViewModels/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace WPR.Mvvm.ViewModels;
+
+/// <summary>
+/// Базовая модель-представление для MVVM.
+/// Также реализует интерфейс <see cref="ObservableRecipient"/> для поддержки сообщений. Сразу активна.
+/// </summary>
+public abstract partial class ViewModel : ObservableRecipient
+{
+    protected ViewModel(IMessenger messenger) : base(messenger)
+    {
+        IsActive = true;
+    }
+
+    protected ViewModel() : this(WeakReferenceMessenger.Default)
+    {
+    }
+
+    /// <summary>
+    /// Заголовок модели-представления.
+    /// </summary>
+    [ObservableProperty] private string _Title = string.Empty;
+
+
+    /// <summary>
+    /// Индикатор того, что модель-представление занята выполнением операции.
+    /// </summary>
+    [ObservableProperty] private bool _IsBusy;
+}
