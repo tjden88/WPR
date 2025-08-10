3ab783ed87f3971fbbc3f4bd20dd754d91556f3b
Denis <Denis@HOME-PC>
2025-08-11 03:53:26 +0500

    Начало рефакторинга изображения - вложения

--- WPR.MVVM/ViewModels/ObservableModel.cs
+++ WPR.MVVM/ViewModels/ObservableModel.cs
@@ -7,7 +7,7 @@ namespace WPR.Mvvm.ViewModels;
 
 public abstract class ObservableModel<T>(T Model) : ObservableObject where T : class
 {
-    protected readonly T Model = Model ?? throw new ArgumentNullException(nameof(Model));
+    protected T Model { get; } = Model ?? throw new ArgumentNullException(nameof(Model));
 
     // преобразование VM -> Model
     public static implicit operator T(ObservableModel<T> vm)
