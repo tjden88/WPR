17455ff30ad7334c207ca0129c39faf0a6b6f118
Денис <dultsevdv@gmail.com>
2025-08-12 16:15:08 +0500

    Перемещение атрибутов

---
+++ WPR.MVVM/Attributes/AutoNotifyCanExecuteChangedAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace WPR.Mvvm.Attributes;
+
+/// <summary>
+/// Автоматически уведомляет об изменении состояния команды с помощью CommandManager.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+public class AutoNotifyCanExecuteChangedAttribute : Attribute
+{
+    public string? CommandName { get; set; }
+
+    public AutoNotifyCanExecuteChangedAttribute() { }
+
+    public AutoNotifyCanExecuteChangedAttribute(string CommandName)
+    {
+        this.CommandName = CommandName;
+    }
+}
---
+++ WPR.MVVM/Attributes/BindAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace WPR.Mvvm.Attributes;
+
+/// <summary>
+/// Привязать свойство EditViewModel к свойству редактируемой модели.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+public class BindAttribute : Attribute
+{
+    public string? TargetPropertyName { get; }
+
+    public BindAttribute() { }
+
+    public BindAttribute(string targetPropertyName)
+    {
+        TargetPropertyName = targetPropertyName;
+    }
+}
