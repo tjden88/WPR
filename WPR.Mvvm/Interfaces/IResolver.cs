4fe5d6237a4212c282961fb1afdcea59cb4b705e
Денис <dultsevdv@gmail.com>
2025-08-12 14:22:05 +0500

    Добавлены новые перегрузки метода `Get` в `IResolver` и `Resolver`

--- WPR.Mvvm/Interfaces/IResolver.cs
+++ WPR.Mvvm/Interfaces/IResolver.cs
@@ -6,7 +6,22 @@
 /// <typeparam name="T"></typeparam>
 public interface IResolver<out T> where T : notnull
 {
+    /// <summary>
+    /// Получить экземпляр из сервисов приложения
+    /// </summary>
     T Get();
+    
+    /// <summary>
+    /// Получить именованный экземпляр из сервисов приложения
+    /// </summary>
+    /// <param name="name">Именованный параметр сервиса</param>
+    T Get(string name);
+
+    /// <summary>
+    /// Получить сконфигурированный экземпляр из коллекции сервисов
+    /// </summary>
+    /// <param name="configure">действие конфигурации</param>
+    T Get(Action<T> configure);
 
     //IDisposable GetScope(out T value); - на будущее, если понадобится поддержка
 }
--- WPR.Mvvm/Services/Resolver.cs
+++ WPR.Mvvm/Services/Resolver.cs
@@ -6,4 +6,13 @@ namespace WPR.Mvvm.Services;
 internal class Resolver<T>(IServiceProvider services) : IResolver<T> where T : notnull
 {
     public T Get() => services.GetRequiredService<T>();
+
+    public T Get(string name) => services.GetRequiredKeyedService<T>(name);
+
+    public T Get(Action<T> configure)
+    {
+        var instance = Get();
+        configure(instance);
+        return instance;
+    }
 }
