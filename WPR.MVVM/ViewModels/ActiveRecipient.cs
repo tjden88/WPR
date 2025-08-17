5ee3b78a3730298cd279adb390adec1aff7b8b8d
Денис <dultsevdv@gmail.com>
2025-08-18 01:32:18 +0500

    Отображение и загрузка чата из сервиса

--- WPR.MVVM/ViewModels/ActiveRecipient.cs
+++ WPR.MVVM/ViewModels/ActiveRecipient.cs
@@ -1,26 +1,21 @@
-﻿using System.Text.Json.Serialization;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Messaging;
 
 namespace WPR.Mvvm.ViewModels;
 
 /// <summary>
 /// Базовая модель-представление для MVVM.
-/// Также реализует <see cref="ObservableRecipient"/> для поддержки сообщений. Сразу активна.
+/// Также реализует <see cref="ObservableRecipient"/> для поддержки сообщений. Сразу активна (готова к приёму сообщений).
 /// </summary>
-public abstract partial class ActiveRecipient : ObservableRecipient
+public abstract partial class ActiveRecipient : InactiveRecipient
 {
-    
-    [JsonIgnore]
-    protected new bool IsActive { get => base.IsActive; set => base.IsActive = value; }
-    
+
     protected ActiveRecipient() : this(WeakReferenceMessenger.Default)
     {
     }
-    
+
     protected ActiveRecipient(IMessenger messenger) : base(messenger)
     {
         IsActive = true;
-        this.InitializeAttributes();
     }
 }
---
+++ WPR.MVVM/ViewModels/InactiveRecipient.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace WPR.Mvvm.ViewModels;
+
+/// <summary>
+/// Базовая модель-представление для MVVM.
+/// Также реализует <see cref="ObservableRecipient"/> для поддержки сообщений. Неактивна при создании. Может отправлять сообщения
+/// </summary>
+public abstract partial class InactiveRecipient : ObservableRecipient
+{
+    
+    [JsonIgnore]
+    protected new bool IsActive { get => base.IsActive; set => base.IsActive = value; }
+    
+    protected InactiveRecipient() : this(WeakReferenceMessenger.Default)
+    {
+    }
+    
+    protected InactiveRecipient(IMessenger messenger) : base(messenger)
+    {
+        this.InitializeAttributes();
+    }
+}
