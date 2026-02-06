2f5ea31d18b48226c87228d1265b05819d2e0868
Denis <Denis@HOME-PC>
2026-02-06 21:40:30 +0500

    Новый конвертер

---
+++ WPR/Converters/EmptyTextToParameterValueConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using WPR.Converters.Base;
+
+namespace WPR.Converters;
+
+/// <summary>
+/// Если текст пуст (string.IsNullOrWhiteSpace()) вернёт значение параметра
+/// </summary>
+[ValueConversion(typeof(string), typeof(string))]
+public class EmptyTextToParameterValueConverter : Converter
+{
+    public override object Convert(object v, Type t, object p, CultureInfo c) =>
+        string.IsNullOrWhiteSpace(v.ToString())
+            ? p : v;
+}
